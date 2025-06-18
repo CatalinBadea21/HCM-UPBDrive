dea8e9b2d770268ccc033999ac2dee3d500e12e4
Catalin Badea <129082335+CatalinBadea21@users.noreply.github.com>
2025-06-18 16:31:35 +0300

    Updated ECU Emulator with pedal capability now

--- Arduino ECU Emulator for CAN/sketch_ECU_emulator_CAN/sketch_ECU_emulator_CAN.ino
+++ Arduino ECU Emulator for CAN/sketch_ECU_emulator_CAN/sketch_ECU_emulator_CAN.ino
@@ -2,9 +2,11 @@
 #include <SPI.h>
 
 #define CAN_CS_PIN 10     /* MCP2515 CS pin */
-#define BRAKE_PIN 3       /* Brake button input */
+#define BRAKE_PIN 8       /* Brake button input */
+#define SWITCH_PIN 9      /* Pot / Pedal switch */
 #define RPM_POT A0        /* RPM potentiometer input */
-#define TPS_POT A1        /* TPS potentiometer input */
+#define TPS_POT A2        /* TPS potentiometer input */
+#define PEDAL_POT A4      /* TPS potentiometer input */
 #define VSS_VALUE 50      /* Vehicle speed sensor value (byte) */
 #define GEAR_VALUE 1      /* Current gear (byte) */
 #define CAN_ID 0x3E8      /* EMU sending CAN ID */
@@ -16,14 +18,16 @@ void setup()
   Serial.begin(115200);
 
   pinMode(BRAKE_PIN, INPUT_PULLUP);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
 
   if (CAN.begin(MCP_STDEXT, CAN_500KBPS, MCP_8MHZ) == CAN_OK)
     Serial.println("CAN BUS Shield init ok!");
   else
-  {
-    Serial.println("CAN BUS Shield init fail");
-    while (1);
-  }
+    while (1)
+    {
+      Serial.println("CAN BUS Shield init fail - reset the board!");
+      delay(500);
+    }
   
   CAN.setMode(MCP_NORMAL);
 }
@@ -34,8 +38,21 @@ void loop()
   int rpm = map(rawRpm, 0, 1023, 0, 14000);
   uint8_t rpmScaled = rpm / 100;
 
-  int rawTps = analogRead(TPS_POT);
+  bool switchPressed = digitalRead(SWITCH_PIN);
+  int rawTps = analogRead(PEDAL_POT);
+
+  if (switchPressed) /* Switch between Pedal and Potentiometer */
+  {
+    rawTps = analogRead(PEDAL_POT);
+    Serial.println("Currently using PEDAL input!");
+  }
+  else
+  {
+    rawTps = analogRead(TPS_POT);
+    Serial.println("Currently using POTENTIOMETER input!");
+  }
   uint8_t tps = map(rawTps, 0, 1023, 0, 100);
+  
 
   bool brakePressed = digitalRead(BRAKE_PIN) == LOW;
   uint8_t brake = brakePressed ? 1 : 0;
