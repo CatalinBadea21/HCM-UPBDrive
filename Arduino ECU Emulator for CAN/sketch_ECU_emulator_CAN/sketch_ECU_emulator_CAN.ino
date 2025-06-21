4abbdbca98f3e8697110c3eaa0d908d2fa867b40
Catalin Badea <129082335+CatalinBadea21@users.noreply.github.com>
2025-06-21 17:17:43 +0300

    Updated mapping for pedal

--- Arduino ECU Emulator for CAN/sketch_ECU_emulator_CAN/sketch_ECU_emulator_CAN.ino
+++ Arduino ECU Emulator for CAN/sketch_ECU_emulator_CAN/sketch_ECU_emulator_CAN.ino
@@ -1,15 +1,18 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
-#define CAN_CS_PIN 10     /* MCP2515 CS pin */
-#define BRAKE_PIN 8       /* Brake button input */
-#define SWITCH_PIN 9      /* Pot / Pedal switch */
-#define RPM_POT A0        /* RPM potentiometer input */
-#define TPS_POT A2        /* TPS potentiometer input */
-#define PEDAL_POT A4      /* TPS potentiometer input */
-#define VSS_VALUE 50      /* Vehicle speed sensor value (byte) */
-#define GEAR_VALUE 1      /* Current gear (byte) */
-#define CAN_ID 0x3E8      /* EMU sending CAN ID */
+#define CAN_CS_PIN 10       /* MCP2515 CS pin */
+#define BRAKE_PIN 8         /* Brake button input */
+#define SWITCH_PIN 9        /* Pot / Pedal switch */
+#define RPM_POT A0          /* RPM potentiometer input */
+#define TPS_POT A2          /* TPS potentiometer input */
+#define PEDAL_POT A4        /* TPS potentiometer input */
+#define VSS_VALUE 50        /* Vehicle speed sensor value (byte) */
+#define GEAR_VALUE 1        /* Current gear (byte) */
+#define PEDAL_MIN_VALUE 180 /* Pedal minimum read value (8 bits) */
+#define PEDAL_MAX_VALUE 860 /* Pedal maximum read value (8 bits) */
+#define PEDAL_WINDOW 100    /* Window for fail-safe */
+#define CAN_ID 0x3E8        /* EMU sending CAN ID */
 
 MCP_CAN CAN(CAN_CS_PIN);
 
@@ -36,7 +39,7 @@ void loop()
 {
   int rawRpm = analogRead(RPM_POT);
   int rpm = map(rawRpm, 0, 1023, 0, 14000);
-  uint8_t rpmScaled = rpm / 100;
+  uint8_t tps, rpmScaled = rpm / 100;
 
   bool switchPressed = digitalRead(SWITCH_PIN);
   int rawTps = analogRead(PEDAL_POT);
@@ -44,15 +47,25 @@ void loop()
   if (switchPressed) /* Switch between Pedal and Potentiometer */
   {
     rawTps = analogRead(PEDAL_POT);
+    tps = map(rawTps, PEDAL_MIN_VALUE, PEDAL_MAX_VALUE, 0, 100);
+    
+    if (tps > 100)
+      tps = 100;
+
     Serial.println("Currently using PEDAL input!");
+
+    if ((rawTps < PEDAL_MIN_VALUE - PEDAL_WINDOW) || (rawTps > PEDAL_MAX_VALUE + PEDAL_WINDOW))
+    {
+      tps = 0;
+      Serial.println("!!! Pedal value out of range! Failsafe mode, TPS set to 0 !!!");
+    }
   }
   else
   {
     rawTps = analogRead(TPS_POT);
+    tps = map(rawTps, 0, 1023, 0, 100);
     Serial.println("Currently using POTENTIOMETER input!");
   }
-  uint8_t tps = map(rawTps, 0, 1023, 0, 100);
-  
 
   bool brakePressed = digitalRead(BRAKE_PIN) == LOW;
   uint8_t brake = brakePressed ? 1 : 0;
